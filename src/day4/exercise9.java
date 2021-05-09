fc47a25e9dc5e8bbbb4410d8274d3fa9e081096f
anahitkarhanian <anahit.karhanyan777@gmail.com>
2021-05-09 19:16:27 +0400

    spiral

--- src/day4/exercise9.java
+++ src/day4/exercise9.java
@@ -8,27 +8,42 @@ public class exercise9 {
         int n = scanner.nextInt();
         int[][] matrix = new int[n][n];
         int left = 0;
-        int right = n-1;
-        int up = 0;
-        int down = n-1;
-        while (right != 0){
+        int right = n - 1;
+        int top = 0;
+        int bottom = n - 1;
+        while (top <= n / 2) {
             for (int j = left; j <= right; j++) {
-                matrix[up][j] = 1;
+                matrix[top][j] = 1;
             }
-            for (int i = up; i <= down; i++) {
-                matrix[i][down] = 1;
+            if (left == 0 ){
+                left += 1;
+            } else {
+                left += 2;
+            }
+            top += 2;
+            right -= 2;
+        }
+        left = 0;
+        right = n - 1;
+        top = 0;
+        bottom = n - 1;
+        while (top <= n / 2) {
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = 1;
             }
             for (int j = right; j >= left; j--) {
-                matrix[right][j] = 1;
+                matrix[bottom][j] = 1;
             }
-            up += 2;
-            for (int i = down; i >= up; i--) {
+            top += 2;
+            for (int i = bottom; i >= top; i--) {
                 matrix[i][left] = 1;
             }
-
-            left += 2;
+            left +=2;
             right -= 2;
-            down -= 2;
+            bottom -= 2;
+        }
+        if(n % 2 == 0){
+            matrix[n/2][n/2-1] = 0;
         }
         for (int i = 0; i < n; i++) {
             System.out.println();
